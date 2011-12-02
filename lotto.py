0de8a2bea0d534917438292e77cf761e8b246691
Alexandre Baaklini <abaaklini@gmail.com>
2011-12-02 15:39:47 -0200

    More one menu to choose between the two games quina and sena

--- lotto.py
+++ lotto.py
@@ -4,7 +4,30 @@
     TODO: 
 """
 from quina import QuinaStats
+from sena import SenaStats
 
 if __name__ == '__main__':
-    quina = QuinaStats('D_QUINA.HTM')
-    quina.screen_interf()
+    done = False
+    
+    while not done :
+    
+        print ('')
+        print ('\033[92m' + "The following lottery games are available: " + '\033[0m')
+        print ('')
+        print ("quina : show statistics for quina lottery game")
+        print ("mega  : show statistics for mega-sena lottery game")
+        print ("done  : exit the program")
+        print ('')
+        cmd = raw_input('\033[92m' + 'Choose the game :' + '\033[0m')
+        print ('')
+
+        if cmd == 'quina':
+            quina = QuinaStats('D_QUINA.HTM')
+            quina.screen_interf()
+        elif cmd == 'mega':
+            sena = SenaStats('d_megasc.htm')
+            sena.screen_interf()
+        elif cmd == 'done' :
+            done = True
+        else :
+            print ("I don't understand the command " + cmd)
